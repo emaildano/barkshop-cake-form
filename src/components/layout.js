3e2c20e16c05b283502d50ea55ea8d05997878b8
Daniel Olson <emaildano@gmail.com>
2019-10-06 19:22:38 -0400

    init

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { Container } from 'reactstrap'
+
 
 import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,22 +26,15 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Container>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
+      </Container>
     </>
   )
 }
